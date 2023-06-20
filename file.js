fd858bddc4b7e12dc9c990aa39b59fa93b983175
Soorjyakanta Sethy <soorjyakant17@gmail.com>
2023-06-20 12:54:55 +0530

    changed files
    changes done in js script  in file.js
    
    Co-Authored-By: Draken057 <137148476+Draken057@users.noreply.github.com>

--- file.js
+++ file.js
@@ -1,1 +1,16 @@
-console.img("Hello world") //new files
+// This is a simple JavaScript code snippet
+
+// Function to display a greeting message
+function sayHello(name) {
+    console.log(`Hello, ${name}!`);
+  }
+  
+  // Function to calculate the square of a number
+  function calculateSquare(number) {
+    return number * number;
+  }
+  
+  // Example usage
+  sayHello("John"); // Output: Hello, John!
+  console.log(calculateSquare(5)); // Output: 25
+  
